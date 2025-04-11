3dda9f58ac599e1dc10ccef14f2db1a9dca32e25
Sugu Sugira <Nelly.Sugu@pega.com>
2025-04-11 17:33:18 -0400

    set dynamic port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import VideosRouter from "./Routes/videos.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // CORS headers middleware
 app.use((req, res, next) => {
